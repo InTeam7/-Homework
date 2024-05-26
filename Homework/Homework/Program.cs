0a4fdb6d52be74253f0447795c6d939e500c4302
DESKTOP-H90FJFU\КАМПУХТЕР <danqa03@gmail.com>
2024-05-26 18:21:55 +0500

    Решение ДЗ Методы Функций

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace Homework
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine(GetMinX(1, 2, 3));
+        Console.WriteLine(GetMinX(0, 3, 2));
+        Console.WriteLine(GetMinX(1, -2, -3));
+        Console.WriteLine(GetMinX(5, 2, 1));
+        Console.WriteLine(GetMinX(4, 3, 2));
+        Console.WriteLine(GetMinX(0, 4, 5));
+
+
+        Console.WriteLine(GetMinX(0, 0, 2) != "Impossible");
+        Console.WriteLine(GetMinX(0, 0, 0) != "Impossible");
+    }
+    private static string GetMinX(int a, int b, int c)
+    {
+        string result = "Impossible";
+        if (a >= 0)
         {
-            Console.WriteLine("Hui");
+            var d = -b / (a * 2.0);
+            if (!double.IsInfinity(d))
+            {
+                result = d.ToString(); // ывф
+            }
         }
+        return result;
     }
 }
